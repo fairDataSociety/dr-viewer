4356cc146054c25270ae6b04d628fdafd2dd4ce6
Mustafa <mustafa@linumlabs.com>
2021-09-02 14:46:41 +0200

    Readme and update package.json

---
+++ README.md
@@ -0,0 +1,4 @@
+This is the demo application for reading consent receipts.
+
+1. First install all packages ```yarn```
+2. Run ```yarn start`` to run the app on port 3000.
--- src/containers/MainWrapper.tsx
+++ src/containers/MainWrapper.tsx
@@ -7,30 +7,28 @@ import ConsentReceipt from '../components/consentReceipt/consentReceipt';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 export interface Props {}
 export default function MainWrapper(props: Props) {
-  const [data, setData] = useState(null);
-  const [podName, setPodName] = useState('');
-  const [showSaveModal, setShowSaveModal] = useState(false);
+
   return (
     <Router>
-    <div className="Main">
-      <Navbar
+      <div className="Main">
+      {/* <Navbar
         file={data}
         setPodName={setPodName}
         setShowSaveModal={setShowSaveModal}
         showSaveModal={showSaveModal}
-      ></Navbar>
-     <Switch>
-        <Route exact path='/'>
-          <Main
-            data={data}
-            setData={setData}
-            podName={podName}
-            showSaveModal={showSaveModal}
-          ></Main>
-        </Route>
-        <Route exact path='/consents/:pod/:directory/:name' component={ConsentReceipt}>
-        </Route>
-      </Switch>
+      ></Navbar> */}
+      <Switch>
+          {/* <Route exact path='/'>
+            <Main
+              data={data}
+              setData={setData}
+              podName={podName}
+              showSaveModal={showSaveModal}
+            ></Main>
+          </Route> */}
+          <Route exact path='/consents/:pod/:directory/:name' component={ConsentReceipt}>
+          </Route>
+        </Switch>
     </div>
     </Router>
   );
